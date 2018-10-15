debb99d71cc0fd6e1eaa39d71bc07e86fa885d0a
siquylee <mywhitelotus@gmail.com>
2018-10-15 12:06:18 +0700

    Complete help feature

--- skills/help.js
+++ skills/help.js
@@ -4,24 +4,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 const app_1 = __importDefault(require("../app"));
 const utils_1 = require("../utils");
-function getHelp(message) {
+function getHelp(message, params) {
     let smb = require('slack-message-builder');
     let msg = smb()
         .attachment()
-        .text(utils_1.l('msg.Help', { user: message.user }));
+        .text(utils_1.l('msg.Help', params));
     msg.button()
         .name("feedback")
         .text(utils_1.l('msg.Feedback'))
         .type("button")
         .url(utils_1.l('msg.Feedback.Url'))
-        .style('primary')
+        .style('default')
         .end();
     msg.button()
         .name("support")
         .text(utils_1.l('msg.Support'))
         .type("button")
         .url(utils_1.l('https://ko-fi.com/siquylee'))
-        .style('danger')
+        .style('primary')
         .end();
     msg.end();
     return msg.json();
@@ -29,15 +29,29 @@ function getHelp(message) {
 module.exports = function (controller) {
     controller.hears(['(.)*'], 'direct_message,direct_mention', function (bot, message) {
         bot.whisper(message, {
-            attachments: [getHelp(message)]
+            attachments: [getHelp(message, { user: `<@${message.user}>` })]
         }, function (err, res) {
             if (err) {
                 app_1.default.instance.getLogger().error(err);
             }
         });
     });
-    controller.on('bot_channel_join', function (bot, message) {
+    controller.on('bot_channel_join,bot_group_join', function (bot, message) {
+        bot.reply(message, {
+            attachments: [getHelp(message, { user: '<!everyone>' })]
+        }, function (err, res) {
+            if (err) {
+                app_1.default.instance.getLogger().error(err);
+            }
+        });
     });
     controller.on('user_channel_join,user_group_join', function (bot, message) {
+        bot.whisper(message, {
+            attachments: [getHelp(message, { user: `<@${message.user}>` })]
+        }, function (err, res) {
+            if (err) {
+                app_1.default.instance.getLogger().error(err);
+            }
+        });
     });
 };
--- src/skills/help.ts
+++ src/skills/help.ts
@@ -1,11 +1,11 @@
 import { SlackController, SlackBot, SlackMessage } from 'botkit';
 import App from '../app';
-import { l, langs } from '../utils';
+import { l } from '../utils';
 
 export = function (controller: SlackController) {
     controller.hears(['(.)*'], 'direct_message,direct_mention', function (bot: any, message: any) {
         bot.whisper(message, {
-            attachments: [getHelp(message)]
+            attachments: [getHelp(message, { user: `<@${message.user}>` })]
         }, function (err: any, res: any) {
             if (err) {
                 App.instance.getLogger().error(err);
@@ -13,31 +13,45 @@ export = function (controller: SlackController) {
         });
     });
 
-    controller.on('bot_channel_join', function (bot, message) {
+    controller.on('bot_channel_join,bot_group_join', function (bot: any, message: any) {
+        bot.reply(message, {
+            attachments: [getHelp(message, { user: '<!everyone>' })]
+        }, function (err: any, res: any) {
+            if (err) {
+                App.instance.getLogger().error(err);
+            }
+        });
     });
 
-    controller.on('user_channel_join,user_group_join', function (bot, message) {
+    controller.on('user_channel_join,user_group_join', function (bot: any, message: any) {
+        bot.whisper(message, {
+            attachments: [getHelp(message, { user: `<@${message.user}>` })]
+        }, function (err: any, res: any) {
+            if (err) {
+                App.instance.getLogger().error(err);
+            }
+        });
     });
 }
 
-function getHelp(message: SlackMessage): string {
+function getHelp(message: SlackMessage, params: any): string {
     let smb = require('slack-message-builder');
     let msg = smb()
         .attachment()
-        .text(l('msg.Help', { user: message.user }));
+        .text(l('msg.Help', params));
     msg.button()
         .name("feedback")
         .text(l('msg.Feedback'))
         .type("button")
         .url(l('msg.Feedback.Url'))
-        .style('primary')
+        .style('default')
         .end();
     msg.button()
         .name("support")
         .text(l('msg.Support'))
         .type("button")
         .url(l('https://ko-fi.com/siquylee'))
-        .style('danger')
+        .style('primary')
         .end();
     msg.end();
     return msg.json();
