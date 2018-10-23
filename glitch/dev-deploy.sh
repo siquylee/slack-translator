b8a077ef9c6de6d938ec8508f18deee1e5a4fc4f
siquylee <mywhitelotus@gmail.com>
2018-10-23 18:00:14 +0700

    Refactor deployment script

--- glitch/dev-deploy.sh
+++ glitch/dev-deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-cd /app
+APP_DIR=/app
+cd $APP_DIR
 PACKAGE=https://www.dropbox.com/s/eupfmm2a8nhd4gh/athena.zip?dl=1
 wget -O athena.zip $PACKAGE
 unzip -o athena.zip -d .
 rm -f athena.zip
-chmod +x /app/glitch/*.sh
+chmod +x $APP_DIR/glitch/*.sh
 refresh
--- glitch/patch.sh
+++ glitch/patch.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
 cp google-translate-token.js ../node_modules/google-translate-token/index.js
-refresh
